a71fc9ada8a52a83936591f3a47dd0486ffa9370
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2025-06-30 14:08:09 -0600

    debug script

---
+++ debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rlwrap -c -D 2 -r jdb -classpath build -sourcepath .:lib280-asn1/src ListDemo
+ 
+ 
---
+++ lab1.md
@@ -0,0 +1,1 @@
+rlwrap jdb -classpath build -sourcepath .:lib280-asn1/src ListDemo
